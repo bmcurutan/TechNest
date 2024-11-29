3e951549b93663eac60cf10105227ad988f00501
Bianca <bmcurutan@gmail.com>
2024-11-28 21:45:13 -0800

    render collection UI dynamically with JSON data

---
+++ Web/Month 2, Week 1/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $.getJSON("./characters.json", function(data) {
+        console.log(data);
+
+        data.results.forEach(function(character) {
+
+            var $cardLink = $("<a></a>", {
+                href: character.link
+            });
+
+            var $cardDiv = $("<div></div>", {
+                class: "child card"
+            });
+
+            var $cardImage = $("<img>", {
+                src: character.image,
+                alt: character.name
+            });
+
+            var $cardHeader = $("<h3></h3>", {
+                text: character.name
+            });
+
+            $cardDiv.append($cardImage).append($cardHeader);
+            $cardLink.append($cardDiv);
+            $("#cards_container").append($cardLink);
+        });
+    }).fail(function() {
+        console.error("Error loading JSON");
+    });
+});
